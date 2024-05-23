516f8916d86354e9c5f9a6b2a483af4daffa2641
Valentina Pich <169078817+Vaal-D4t4Sc@users.noreply.github.com>
2024-05-22 23:41:17 -0300

    Update and rename 51consignas.py to 511consignas.py

---
+++ Ejercicios/511consignas.py
@@ -0,0 +1,32 @@
+#Identificar situaciones donde se utilice la estructura condicional para cada uno de estos casos:
+
+#1. Condicional parcial (solo el if) con expresión lógica simple
+#  a = 1
+#  b = 2
+#  if b > a:
+#     print("b es mayor que a")
+
+#2. Condicional completo (if - else) con expresión lógica simple
+# Edad = int (input ("Ingresar edad:"))
+# if Edad >= 18:
+#   print ("Eres mayor de edad.")
+# else:
+#   print ("Eres menor de edad.")
+
+#3. Condicional parcial (solo el if) con expresión lógica compuesta (and)
+# importe = float (input ("Ingrese el importe a evaluar: "))
+# print()
+# if importe > 10000:
+#    print ("Debe registrar su DNI")
+#    DNI = int (input())
+#    print("Gracias por su compra" , DNI)
+# else : 
+#    print ("Gracias por su compra Don Cliente")
+
+#4. Condicional completo (if - else) con expresión lógica compuesta (or)
+# dia = input ("Ingrese el día de la semana: ")
+# if dia == "sabado" or dia == "domingo":
+#    print ("Feliz fin de semana")
+# else:
+#    print ("Hoy debe ir al colegio")
+
